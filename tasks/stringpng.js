f3779d47a71c26d996d4d45afdc509f960110605
mog <mog@trbl.at>
2013-07-06 10:10:38 +0200

    init
    This saves Strings as colourvalues into PNG, where the value for one
    char is used for RGB and transparency is set to 255 (~none)

---
+++ tasks/stringpng.js
@@ -0,0 +1,48 @@
+/*
+ * grunt-stringpng
+ * http://trbl.at/
+ *
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    var fs = require('fs'),
+        PNG = require('pngjs').PNG;
+
+    grunt.registerMultiTask('stringpng', 'String to PNG', function() {
+
+        var done = this.async(),
+            destURL = this.files[0].dest;
+
+
+        var src = (grunt.file.read(this.filesSrc)).trim(),
+            dst = fs.createWriteStream(destURL),
+            png = new PNG({
+                height: 1,
+                width: src.length
+                //deflateLevel:0,
+                //deflateStrategy:0,
+                //filterType:-1
+            });
+
+        for(var i = 0, c = 0; i < src.length * 4; i+=4, c++){
+
+           var char = src.charCodeAt(c);
+            png.data[i] = char;
+            png.data[i+1] = char;
+            png.data[i+2] = char;
+
+            png.data[i+3] = 255;
+        }
+
+        var stream = png.pack();
+        stream.on('end', function(){
+            done(0);
+        });
+
+        stream.pipe(dst);
+    });
+};
